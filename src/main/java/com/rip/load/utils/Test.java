62e3eafae62bdc7878634c5352b2f61e84412171
zxh <neilxiaohui@163.com>
2019-05-09 14:15:51 +0800

    等额本息，等额本金的计算工具类

--- src/main/java/com/rip/load/utils/Test.java
+++ src/main/java/com/rip/load/utils/Test.java
@@ -2,42 +2,39 @@ package com.rip.load.utils;
 
 import javax.management.ObjectName;
 import javax.sql.rowset.CachedRowSet;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledFuture;
 
 public class Test {
 
-    public static void main(String[] args) {
-        int i = 0 ;
-        int k[] = new int[]{1,2,3};
-
-        int length = k.length;
-        Object o = new Object();
-        o.notify();
-        System.out.println(i++);
-        //result:0
-
-        System.gc();
-
-        System.out.println(i);
-        //result:1
-
-        int j = 0;
-        j++;
-        System.out.println(j);
-
-        List<String> list = new ArrayList<>();
-        list.add("1");
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("1", 1);
-
-
-        //result:1
+    public static double getPerMonthPrincipalInterest(double invest, double yearRate, int totalmonth) {
+        double monthRate = yearRate;
+        BigDecimal monthIncome = new BigDecimal(invest)
+                .multiply(new BigDecimal(monthRate * Math.pow(1 + monthRate, totalmonth)))
+                .divide(new BigDecimal(Math.pow(1 + monthRate, totalmonth) - 1), 2, BigDecimal.ROUND_DOWN);
+        return monthIncome.doubleValue();
     }
+
+    public static void main(String[] args) {
+
+        double perMonthPrincipalInterest = Test.getPerMonthPrincipalInterest(100, 0.01, 6);
+        System.out.println("1:   " + perMonthPrincipalInterest);
+
+
+        double A = 100;
+        double p = 0.01;
+        double k = 6.00;
+
+            System.out.println(
+                    (A*p*Math.pow((1+p), k))/
+                            (Math.pow((1+p), k) - 1)
+            );
+
+        System.out.println(new BigDecimal(106).divide(new BigDecimal(6), 2, BigDecimal.ROUND_DOWN));
+    }
+
 }
